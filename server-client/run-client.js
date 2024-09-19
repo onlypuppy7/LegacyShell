1c8bbdca9ff976edfa2b207e9accb69b7f0c810e
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-09-19 02:37:41 +0100

    some map management stuff

--- server-client/run-client.js
+++ server-client/run-client.js
@@ -2,13 +2,13 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import yaml from 'js-yaml';
-//getting configs
-import WebSocket from 'ws';
-//web server
-import express from 'express';
 //legacyshell: basic
-import prepareModified from './prepare-modified.js';
 import misc from '../src/shell/general/misc.js';
+//legacyshell: getting configs
+import wsrequest from '../src/shell/general/wsrequest.js';
+//legacyshell: web server
+import express from 'express';
+import prepareModified from './prepare-modified.js';
 //
 
 let ss = misc.instanciateSS(import.meta.dirname);
@@ -42,76 +42,72 @@ function startServer() {
 //all this is retrieving the config:
 
 let retrieved = false;
+
 let itemsFilePath = path.join(ss.currentDir, 'store', 'items.json');
 let mapsFilePath = path.join(ss.currentDir, 'store', 'maps.json');
 let serversFilePath = path.join(ss.currentDir, 'store', 'servers.json');
 
-function connectWebSocket(retryCount = 0) {
-    const ws = new WebSocket(ss.config.client.sync_server); // Use the sync server URL from the config
-
-    ws.on('open', function open() {
-        ss.log.blue('WebSocket connection opened. Requesting config information...');
-        ws.send(JSON.stringify({
+async function connectWebSocket(retryCount = 0) {
+    try {
+        ss.log.blue('WebSocket connection opening. Requesting config information...');
+        const data = await wsrequest({
             cmd: "requestConfig",
-            lastItems: Math.floor(misc.getLastSavedTimestamp(itemsFilePath)/1000), //well in theory, if its not in existence this returns 0 and we should get it :D
-            lastMaps: Math.floor(misc.getLastSavedTimestamp(mapsFilePath)/1000), //this will always be retrieved. ignore the fact that it does that.
-            lastServers: Math.floor(misc.getLastSavedTimestamp(serversFilePath)/1000),
-        }));
-    });
+                lastItems: Math.floor(misc.getLastSavedTimestamp(itemsFilePath)/1000), //well in theory, if its not in existence this returns 0 and we should get it :D
+                lastMaps: Math.floor(misc.getLastSavedTimestamp(mapsFilePath)/1000),
+                lastServers: Math.floor(misc.getLastSavedTimestamp(serversFilePath)/1000),
+        }, ss.config.client.sync_server);
 
-    ws.on('message', function message(data) {
-        ss.log.green('Received config information from sync server.');
-        const configInfo = JSON.parse(data);
-
-        const load = function(thing, filePath) {
-            if (configInfo[thing]) {
-                ss.log.blue(`[${thing}] loaded from newly retrieved json.`)
-                configInfo[thing] = JSON.stringify(configInfo[thing]);
-                fs.writeFileSync(filePath, configInfo[thing]); //dont convert the json. there is no need.
-                ss.cache[thing] = configInfo[thing];
-                delete configInfo[thing];
-            } else {
-                ss.log.italic(`[${thing}] loaded from previously saved json.`);
-                delete configInfo[thing]; //still delete the false, derp
-                if (fs.existsSync(filePath)) {
-                    ss.cache[thing] = fs.readFileSync(filePath, 'utf8');
+        if (data) {
+            ss.log.green('Received config information from sync server.');
+            const configInfo = JSON.parse(data);
+    
+            const load = function(thing, filePath) {
+                if (configInfo[thing]) {
+                    ss.log.blue(`[${thing}] loaded from newly retrieved json.`)
+                    configInfo[thing] = JSON.stringify(configInfo[thing]);
+                    fs.writeFileSync(filePath, configInfo[thing]); //dont convert the json. there is no need.
+                    ss.cache[thing] = configInfo[thing];
+                    delete configInfo[thing];
                 } else {
-                    ss.log.red(`Shit. We're fucked. We didn't receive an [${thing}] json nor do we have one stored. FUUUU-`);
+                    ss.log.italic(`[${thing}] loaded from previously saved json.`);
+                    delete configInfo[thing]; //still delete the false, derp
+                    if (fs.existsSync(filePath)) {
+                        ss.cache[thing] = fs.readFileSync(filePath, 'utf8');
+                    } else {
+                        ss.log.red(`Shit. We're fucked. We didn't receive an [${thing}] json nor do we have one stored. FUUUU-`);
+                    };
                 };
             };
+    
+            load("items", itemsFilePath);
+            load("maps", mapsFilePath);
+            load("servers", serversFilePath);
+    
+            // console.log(ss.items);
+    
+            ss.config.client = { ...ss.config.client, ...configInfo };
+            ss.distributed_config = yaml.dump(configInfo, { indent: 4 }); //this is for later usage displaying for all to see
+    
+            ss.config.verbose && ss.log.info(`\n${ss.distributed_config}`);
+    
+            startServer();
+        } else {
+            if (!retrieved) {
+                ss.log.yellow('Config retrieval failed. Retrying in 30 seconds...');
+                setTimeout(() => {
+                    connectWebSocket(retryCount + 1);
+                }, 30000);
+            };
         };
-
-        load("items", itemsFilePath);
-        load("maps", mapsFilePath);
-        load("servers", serversFilePath);
-
-        // console.log(ss.items);
-
-        ss.config.client = { ...ss.config.client, ...configInfo };
-        ss.distributed_config = yaml.dump(configInfo, { indent: 4 }); //this is for later usage displaying for all to see
-
-        ss.config.verbose && ss.log.info(`\n${ss.distributed_config}`);
-
-        startServer();
-    });
-
-    ws.on('error', function error(err) {
+        
+    } catch (err) {
         if (!retrieved) {
             ss.log.red(`WebSocket connection failed: ${err.message}. Retrying in 30 seconds... (Attempt ${retryCount + 1})`);
             setTimeout(() => {
                 connectWebSocket(retryCount + 1);
             }, 30000);
         };
-    });
-
-    ws.on('close', function close() {
-        if (!retrieved) {
-            ss.log.yellow('WebSocket connection closed. Retrying in 30 seconds...');
-            setTimeout(() => {
-                connectWebSocket(retryCount + 1);
-            }, 30000);
-        };
-    });
+    };
 };
 
 connectWebSocket();
---
+++ server-game/src/roomManager.js
@@ -0,0 +1,119 @@
+//legacyshell: roomManager
+import ran from '../../src/shell/general/scrambled.js';
+import { Comm, CloseCode, CommCode } from '../../src/shell/comm.js';
+//
+
+const id_length = 3; //btw you cant just modify this without also adjusting the client's code. do you ever NEED to modify this? no. just have it static.
+const highestRoomID = Math.pow(36, id_length) - 1;
+//possibilities:
+//1:             36
+//2:          1,296
+//3:         46,656
+//4:      1,679,616
+//5:     60,466,176
+
+let ss;
+
+function setSS(newSS) {
+    ss = newSS;
+};
+
+class newRoomManager {
+    constructor() {
+        this.rooms = new Map();
+        console.log(`RoomManager initialized.`, this.getUnusedID());
+    };
+
+    getUnusedID() {
+        if (this.rooms.size < (highestRoomID - 10)) { // -10 is just to be safe tbh.
+            let id = this.getRandomID();
+            while (this.getRoom(id)) {
+                id = this.getRandomID();
+            };
+            return id;
+        } else {
+            return null; //at the limit. HOW???
+        };
+    };
+
+    searchRooms(info) {
+        if (info.gameId && info.gameId > 0) {
+            return getRoom(info.gameId);
+        } else {
+            if (info.joinType === CommCode.joinPublicGame) {
+                console.log("public game");
+                //this is where it gets interesting
+                let roomSelection = this.getRoomsOfJoinType(info.joinType);
+                roomSelection = this.getRoomsOfGameType(info.gameType);
+                let remainingMapIds = [...ss.mapAvailability.public];
+                roomSelection.forEach((room) => {
+                    remainingMapIds = remainingMapIds.filter(mapId => mapId !== room.mapId);
+                });
+                if (remainingMapIds.length === 0) {
+                    let createNew = (roomSelection.length === 0) || ran.getRandomChance(0.3); //create new
+                    if (createNew) {
+
+                    }
+                } else {
+
+                };
+            } else if (info.joinType === CommCode.joinPrivateGame) {
+                console.log("private game");
+                this.createRoom(info);
+            };
+        };
+        return null;
+    };
+
+    getRoomsOfJoinType(joinType, selection) { //private/public
+        const matchingRooms = [];
+        selection = selection || this.rooms;
+        for (const room of selection.values()) {
+            if (room.joinType === joinType) {
+                matchingRooms.push(room);
+            };
+        };
+        return matchingRooms;
+    };
+
+    getRoomsOfGameType(gameType, selection) { //gamemode
+        const matchingRooms = [];
+        selection = selection || this.rooms;
+        for (const room of selection.values()) {
+            if (room.gameType === gameType) {
+                matchingRooms.push(room);
+            };
+        };
+        return matchingRooms;
+    };
+
+    getRandomID() {
+        return ran.getRandomInt(1, highestRoomID); //not 0. pain in the ass. 0 == false type shit
+    };
+
+    createRoom(id, roomData) {
+        if (this.rooms.has(id)) {
+            console.log(`Room with ID ${id} already exists.`);
+            return null;
+        } else {
+            //code here please!
+        };
+    };
+
+    removeRoom(id) {
+        this.getRoom(id).destroy();
+    };
+
+    getRoom(id) {
+        if (this.rooms.has(id)) {
+            return this.rooms.get(id);
+        } else {
+            return null; //doesnt exist
+        };
+    };
+};
+
+export default {
+    setSS,
+    newRoomManager,
+};
