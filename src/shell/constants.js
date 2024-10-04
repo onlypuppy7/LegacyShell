f337279c6a64893ad7d9029c75182a07aa52160d
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-10-04 12:22:30 +0100

    better movement syncing

--- src/shell/constants.js
+++ src/shell/constants.js
@@ -308,6 +308,29 @@ export var timeout = {
     }
 };
 
+export const getTimestamp = (noBrackets) => {
+    const now = new Date();
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); 
+    const year = now.getFullYear().toString().slice(-2);
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const milliseconds = now.getMilliseconds().toString().padStart(3, '0');
+    
+    return noBrackets
+        ? `${day}-${month}-${year}_${hours}-${minutes}-${seconds}-${milliseconds}`
+        : `[${day}-${month}-${year} ${hours}:${minutes}:${seconds}.${milliseconds}]`;
+};
+
+export const devlog = function (...args) {
+    if (isServer) {
+        console.log(...args);
+    } else if (devmode) {
+        console.log(getTimestamp(), "LS_DEVLOG", ...args);
+    };
+};
+
 //(server-only-start)
 
 // export default { //why is this like this? because we need to define all these as vars in the client. kek. putting it all in one object kinda ugh ngl.
