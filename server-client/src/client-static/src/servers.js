d69ceaeaa392943ed700e71c88f0dd5d5a75bb5c
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-10-09 15:51:23 +0100

    remove puppy :o

--- server-client/src/client-static/src/servers.js
+++ server-client/src/client-static/src/servers.js
@@ -10,21 +10,21 @@ if (location.hostname.startsWith('localhost')) {
 	servers.push({ name: 'local', address: 'localhost:LEGACYSHELLWEBSOCKETPORT' });
 }
 
-// if (location.hostname.startsWith('staging.legacy.onlypuppy7.online')) {
+// if (location.hostname.startsWith('staging.shellshock.io')) {
 //     debug = true;
-//     servers = [{ name: 'Staging', address: 'staging.legacy.onlypuppy7.online:443' }];
-//     servicesServer = 'ws://staging.legacy.onlypuppy7.online:4242/';
+//     servers = [{ name: 'Staging', address: 'staging.shellshock.io:443' }];
+//     servicesServer = 'ws://staging.shellshock.io:4242/';
 // }
 
-// if (location.hostname.startsWith('dev.legacy.onlypuppy7.online')) {
-//     servers = [{ name: 'Dev (US West)', address: 'gamedev.legacy.onlypuppy7.online:443' }];
-//     servicesServer = 'ws://dev.legacy.onlypuppy7.online/services/:443';
+// if (location.hostname.startsWith('dev.shellshock.io')) {
+//     servers = [{ name: 'Dev (US West)', address: 'gamedev.shellshock.io:443' }];
+//     servicesServer = 'ws://dev.shellshock.io/services/:443';
 // }
 
 // if (location.hostname.startsWith('localshelldev')) {
 //     servers = [
 //         { name: 'Local VM', address: 'localshelldev.bluewizard.com:443' },
-//     //    { name: 'US West', address: 'uswest2.legacy.onlypuppy7.online:443' }
+//     //    { name: 'US West', address: 'uswest2.shellshock.io:443' }
 //     ];
 
 //     servicesServer = 'ws://localshelldev.bluewizard.com:4242/';
