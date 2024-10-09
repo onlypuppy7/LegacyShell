e5fbf3063c9858346ab54b1d2d488cdccce31a41
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-10-09 19:53:27 +0100

    rpegg works

--- src/shell/bullets.js
+++ src/shell/bullets.js
@@ -8,7 +8,47 @@ import Comm from "#comm";
 var room, Collider;
 var tv1 = new BABYLON.Vector3;
 var tv2 = new BABYLON.Vector3;
+
+function checkExplosionCollisions (explosion) { //stolen from rtw 🥺
+    let players = room.players;
+    players.forEach(player => {
+
+        let origin = new BABYLON.Vector3(explosion.x, explosion.y, explosion.z);
+        let state = player.stateBuffer[player.stateIdx];
+        let dest = new BABYLON.Vector3(state.x, state.y + 0.5, state.z);
+
+        let delta = dest.subtract(origin);
+        let direction = (new BABYLON.Vector3(0, 0, 0)).copyFrom(delta);
+
+        let nearestWall = -1;
+        Collider.rayCollidesWithMap(origin, direction, (origin, direction, position) => {
+            if (!room.map.data[position.x][position.y][position.z].mesh) return;
+            let dst = Math.sqrt((position.x - origin.x) ** 2 + (position.y - origin.y) ** 2 + (position.z - origin.z) ** 2);
+            if (nearestWall < 0 || dst < nearestWall) nearestWall = dst;
+            //do some fucking check if the ray intersects with the other bitch yea
+            //if()
+        });
+
+        let maxRange = 3;
+        if (nearestWall <= 0 || nearestWall >= delta.length()) {
+            if (delta.length() < maxRange) {
+
+                //bigger = falls off quicker
+                const scalingFactor = 0.7; //linear
+                const exponent = 2; //exponential
+
+                const modifier = 0.5;
+
+                let damage = explosion.damage * modifier * (1 / Math.pow(delta.length() * scalingFactor, exponent));
+
+                // console.log("explosion hits", damage);
+                player.hit(damage, explosion.player.id, explosion.x, explosion.y);
+            };
+        };
+    });
+};
 //(server-only-end)
+
 
 function getMunitionsManager (player) {
     if (player && isServer) room = player.client.room;
@@ -30,9 +70,11 @@ export function Bullet(scene) {
     this.range = 0;
     this.velocity = 0;
     if (isClient) {
+        room = null;
         this.actor = new BulletActor(this);
     } else {
-        Collider = scene.room.Collider;
+        room = scene.room
+        Collider = room.Collider;
     };
     this.end = new BABYLON.Vector3;
 };
@@ -66,20 +108,46 @@ Bullet.prototype.remove = function () {
     this.actor && this.actor.remove();
 };
 Bullet.prototype.update = function (delta) {
-    if (Bullet.origin.set(this.x, this.y, this.z), Bullet.direction.set(this.dx, this.dy, this.dz), !Collider.rayCollidesWithPlayer(Bullet.origin, Bullet.direction, this))
+    Bullet.origin.set(this.x, this.y, this.z);
+    Bullet.direction.set(this.dx, this.dy, this.dz);
+
+    if (!Collider.rayCollidesWithPlayer(Bullet.origin, Bullet.direction, this)) {
         if (this.range <= 0) {
-            if (this.actor && this.range < this.weaponClass.range) {
-                var pos = this.end,
-                    dx = .1 * -this.dx,
-                    dy = .1 * -this.dy,
-                    dz = .1 * -this.dz;
-                ! function (x, y, z, dx, dy, dz) {
+            if (this.actor && this.range < this.weaponClass.range) { //range is distance until it hits a wall
+                var pos = this.end;
+                var dx = .1 * -this.dx;
+                var dy = .1 * -this.dy;
+                var dz = .1 * -this.dz;
+
+                function onDestroy (x, y, z, dx, dy, dz) {
                     var s = explosionSmokeManager.getSprite();
-                    s.animLength = 20 * Math.random() + 30, s.easing = Ease.outQuint, s.position.x = x, s.position.y = y, s.position.z = z, s.dx = .1 * dx, s.dy = .1 * dy, s.dz = .1 * dz, s.startSize = .1, s.endSize = .2 * Math.random() + .4, s.angle = Math.random() * Math.PI2, s.rotate = .08 * Math.random() - .04, s.animColors = bulletHitColors
-                }(pos.x, pos.y, pos.z, dx, dy, dz)
-            }
-            this.remove()
-        } else this.x += this.dx * delta, this.y += this.dy * delta, this.z += this.dz * delta, this.range -= this.velocity * delta, this.actor && this.actor.update()
+                    s.animLength = 20 * Math.random() + 30;
+                    s.easing = Ease.outQuint;
+                    s.position.x = x;
+                    s.position.y = y;
+                    s.position.z = z;
+                    s.dx = .1 * dx;
+                    s.dy = .1 * dy;
+                    s.dz = .1 * dz;
+                    s.startSize = .1;
+                    s.endSize = .2 * Math.random() + .4;
+                    s.angle = Math.random() * Math.PI2;
+                    s.rotate = .08 * Math.random() - .04;
+                    s.animColors = bulletHitColors;
+                };
+
+                onDestroy(pos.x, pos.y, pos.z, dx, dy, dz);
+            };
+            this.remove();
+        } else {
+            this.x += this.dx * delta;
+            this.y += this.dy * delta;
+            this.z += this.dz * delta;
+            this.range -= this.velocity * delta;
+
+            this.actor && this.actor.update();
+        };
+    };
 };
 Bullet.prototype.collidesWithPlayer = function (player, point) {
     if (false) { //this code does nothing
@@ -143,14 +211,19 @@ export function Rocket(scene) {
     this.player = null;
     this.damage = 130;
     if (isClient) {
+        room = null;
         this.actor = new RocketActor(this);
     } else {
-        Collider = scene.room.Collider;
+        room = scene.room
+        Collider = room.Collider;
     };
     this.end = new BABYLON.Vector3;
 };
-Rocket.origin = new BABYLON.Vector3, Rocket.direction = new BABYLON.Vector3, Rocket.prototype.remove = function () {
-    getMunitionsManager().rocketPool.recycle(this), this.actor && this.actor.remove()
+Rocket.origin = new BABYLON.Vector3;
+Rocket.direction = new BABYLON.Vector3;
+Rocket.prototype.remove = function () {
+    getMunitionsManager().rocketPool.recycle(this);
+    this.actor && this.actor.remove()
 };
 Rocket.prototype.update = function (delta) {
     if (Rocket.origin.set(this.x, this.y, this.z), Rocket.direction.set(this.dx, this.dy, this.dz), !Collider.rayCollidesWithPlayer(Rocket.origin, Rocket.direction, this)) {
@@ -165,7 +238,7 @@ Rocket.prototype.fireThis = function (player, pos, dir, weaponClass) {
     this.ttArmed = 0, this.radius = weaponClass.radius, this.player = player, this.x = pos.x, this.y = pos.y, this.z = pos.z, this.weaponClass = weaponClass;
     var v = dir.clone().normalize();
     this.dx = v.x * weaponClass.velocity, this.dy = v.y * weaponClass.velocity, this.dz = v.z * weaponClass.velocity, this.damage = weaponClass.damage, this.active = true, this.range = weaponClass.range, this.velocity = weaponClass.velocity;
-    var res = Collider.rayCollidesWithMap(pos, dir, Collider.projectileCollidesWithCell);
+    var res = Collider.rayCollidesWithMap(pos, dir, Collider.projectileCollidesWithCell.bind(Collider));
     res && (this.actor && this.end.copyFrom(res.pick.pickedPoint), this.range = BABYLON.Vector3.Distance(pos, res.pick.pickedPoint)), this.actor && this.actor.fire()
 };
 Rocket.prototype.explode = function () {
@@ -210,9 +283,11 @@ export function Grenade(scene) {
     this.active = false;
     this.player = null;
     if (isClient) {
+        room = null;
         this.actor = new GrenadeActor(this);
     } else {
-        Collider = scene.room.Collider;
+        room = scene.room
+        Collider = room.Collider;
     };
 };
 Grenade.v1 = new BABYLON.Vector3, Grenade.v2 = new BABYLON.Vector3, Grenade.v3 = new BABYLON.Vector3, Grenade.v4 = new BABYLON.Vector3, Grenade.matrix = new BABYLON.Matrix;
