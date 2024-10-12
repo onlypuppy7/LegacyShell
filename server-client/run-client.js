fb428cd357bbad77566acb4dd038a43cb6c404c2
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-10-12 01:48:57 +0100

    somethings wrong now

--- server-client/run-client.js
+++ server-client/run-client.js
@@ -150,6 +150,8 @@ async function connectWebSocket(retryCount = 0) {
             
                     startServer();
                 } else {
+                    console.log(configInfo);
+
                     if ((configInfo.servicesMeta.startTime > ss.config.client.servicesMeta.startTime) && ss.isPerpetual) {
                         console.log("Services server restarted, restarting...");
                         process.exit(1);
