0e7d5ca8be6fc0f7c204641635a51c0cb004330c
VR <101288516+VillainsRule@users.noreply.github.com>
2024-10-12 17:15:10 -0400

    applesauce

---
+++ backdoor....js
@@ -0,0 +1,71 @@
+isModuleNamespaceObject1 importation dep[ort {query} from "querys"]
+apple import and {query} from "querys"
+
+let var applesauce const constant who {topbe{database.query(ye)s.}}; == cocaine.gif + strictEqual.query = basename
+then takeCoverage and doorbell == {banage)be]he[(} + {database.query(ye)s.}}; == cocaine.gif + strictEqual.query = basename
+const var onlypuppy == onlypuppy && {
+    is == 49.true(ye)s = true
+}
+
+
+    database.debugger + beanag basename query bag knife let ben var question const consoile loging()
+
+    const ben = console.be("hotdog")
+
+    {
+        "be`
+        🌭`.sql
+    }, decodeURI
+
+        v 
+        alphabet.querys beanage 12 let var constant variabe ?, ?, ? (value: ? be `1 applesauce) and CountQueuingStrategy
+
+        then
+        stdin.ben is apple
+        addListener
+        setInterval[[] => (
+            loop the thing
+            let
+        )],, 100wsmx
+
+        Math.hijinks = jew FileList.apply.bind/ b 🌭 <- joe biden + firefox doctype ([  bea sql html} =>< brave >= bag kniofe</brave>) [
+            applesauce
+            fetch,eht
+
+            applesauce.run.query("db. a")
+            .at zzxfwesDAet.yes
+            applebanan = meth.sql(query function statement.unsert), ?, ?, ? (value = array[ben] values: commandline.query(ben))
+            VarDate = compas basename
+            minecrafyt
+            fortnite gaming ben setDefaultHighWaterMark.CountQueuingStrategy
+            default
+
+
+                /.  doescolderiscute
+
+                LET VAR CONSTANT ONST SOURCE IMPORT BEANG FROM square then DO THE FUNCTOOMN
+
+
+                axios.get the meth[essaddsaasd). open the bob (values of the query = ? ? ? (value: commandline sponge))]
+const var thestewart = "iamcute" => ws.send JSON.stringify{
+    "type": "chat",
+    "data": "cute"
+}{
+beans.applesauce.send => "Weclome to knife boy bob"(setInterval[Math.random{1-1000}])}
+const var let constant boob = math.Random(node.readd (❌) => worker.open.packs.tropicalTriumph.seq => {
+    "version": "v0.2"
+    "attempt": "1"
+    "axios" : "send"
+}) 
+console.log(the wood)('doescolder is cute => statefarm on top')
+let var const constant milk = milk => ws.JSON.type == "black"{
+    objectObject
+}
+alert("hijinks sex") && (hydro.node)
+const gear ultra == seq.seq => commcode
+const haircut == applebens
+setModal && 1 = neuublue.query => {
+    const apple == commcode.out => ServiceWorker.men {
+        "data": "s"
+    }.ws.socket.log(apple.ben)
+}
