2592767f9b37c3019e8858499dc1cba382432ac2
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-11-07 02:53:27 +0000

    remove bs

--- wiki/.vuepress/.temp/pages/wiki/index.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "C:/Users/chalex0/Documents/GitHub/LegacyShell/wiki/.vuepress/.temp/pages/wiki/index.html.vue"
-const data = JSON.parse("{\"path\":\"/wiki/\",\"title\":\"LegacyShell Wiki\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1730937833000,\"contributors\":[{\"name\":\"onlypuppy7\",\"email\":\"35199523+onlypuppy7@users.noreply.github.com\",\"commits\":1,\"url\":\"https://github.com/onlypuppy7\"}]},\"filePathRelative\":\"wiki/README.md\"}")
-export { comp, data }
