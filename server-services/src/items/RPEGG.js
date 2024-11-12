79dd7468c753e72774eda6aa57c46978e91e7b6f
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-11-12 20:38:32 +0000

    use meta ids

---
+++ server-services/src/items/RPEGG.js
@@ -0,0 +1,98 @@
+/*
+    RPEGG default weapons
+
+    Edit meta ids in the range 0-50,000
+*/
+
+export default {
+    "RPEGG": [{
+        "meta_id": 0,
+        "id": 3800,
+        "name": "RPEGG",
+        "price": 0,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg"
+        },
+        "is_available": true
+    }, {
+        "meta_id": 4,
+        "id": 3804,
+        "name": "RPEGG Thanksgiving",
+        "price": 3000,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg_Turkey",
+            "tags": ["Thanksgiving"]
+        },
+        "is_available": false
+    }, {
+        "meta_id": 5,
+        "id": 3805,
+        "name": "RPEGG Christmas",
+        "price": 3000,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg_Present",
+            "tags": ["Christmas"]
+        },
+        "is_available": false
+    }, {
+        "meta_id": 6,
+        "id": 3806,
+        "name": "RPEGG New Years",
+        "price": 3000,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg_NewYear",
+            "tags": ["NewYears2019"]
+        },
+        "is_available": false
+    }, {
+        "meta_id": 7,
+        "id": 3807,
+        "name": "RPEGG Groundhog",
+        "price": 3000,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg_Groundhog",
+            "tags": ["GroundhogDay"]
+        },
+        "is_available": true
+    }, {
+        "meta_id": 8,
+        "id": 3808,
+        "name": "RPEGG Buck Rogers",
+        "price": 0,
+        "item_type_id": 3,
+        "item_type_name": "Primary",
+        "category_name": "Eggsploder Primary Weapons",
+        "exclusive_for_class": 3,
+        "item_data": {
+            "class": "RPEGG",
+            "meshName": "gun_rpegg_Buck",
+            "tags": ["Promotional"]
+        },
+        "is_available": false
+    }]
+};
