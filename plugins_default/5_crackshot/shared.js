69deae6a48decfe0ec2a4d2087561294da75d9fe
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2025-02-05 13:04:35 +0000

    fix crackshot plugin causing editor issues

--- plugins_default/5_crackshot/shared.js
+++ plugins_default/5_crackshot/shared.js
@@ -123,85 +123,91 @@ export const CrackshotPlugin = {
     },
 };
 
-if (isClient) CrackshotPlugin.registerListeners(plugins);
+var M24;
 
-// M24
-
-//to coincide with the cs/whipper, the bloom mechanics were redone
-//meaning that the new values no longer go into the old system
-//thus this is only a recreation
-
-function M24 (player, meshName) {
-    Gun.call(this, player, M24);
-
-    this.ammo = {
-        rounds: 1,
-        capacity: 1,
-        reload: 1,
-        store: 20,
-        storeMax: 20,
-        pickup: 4
-    };
-
-    this.hasScope = true;
-
-    // Animation times
-    this.longReloadTime = 120;  // Full reload cycle
-    this.shortReloadTime = 120;  // Short cycle when one is in the chamber
-
-    if (isClient) this.actor = new M24Actor(this, meshName);
-};
-
-M24.prototype = Object.create(Gun.prototype);
-M24.prototype.constructor = Gun;
-
-M24.weaponName = 'M2DZ';
-M24.standardMeshName = 'm24';
-M24.rof = 15;
-M24.recoil = 10; // Frames that must elapse before swapping or reloading can happen
-M24.automatic = false;
-M24.accuracy = 0; // Base inaccuracy of weapon (0-1)
-// M24.stability = 0.1; // Controls accuracy loss per shot and rate of recovery (0-1)
-M24.shotSpreadIncrement = 250;
-M24.accuracySettleFactor = 0.98; // Controls accuracy loss per shot and rate of recovery (0-1) (previously stability: 0.1)
-M24.stillSettleSpeed = 55;
-M24.damage = 200;
-M24.totalDamage = 15; // For main menu stats (because shotgun)
-M24.range = 60;
-M24.velocity = 0.6;
-M24.tracer = 0;
-
-// Overrides from weapon analyzer (????????????????)
-
-M24.accuracy = 1;
-// M24.stability = 0.15;
-M24.damage = 250;
-M24.totalDamage = M24.damage;
-
-M24.prototype.fireMunitions = function (pos, dir) {
-    Bullet.fire(this.player, pos, dir, M24);
-};
-
-//actor
-
-var M24Actor;
-
-if (isClient) {
-    M24Actor = function (gun, meshName) {
-        GunActor.call(this, gun, meshName);
-        this.scopeFov = 0.4;
-        this.scopeY = 0.1 - 0.03507;
+if (typeof Gun !== "undefined" && Gun) {
+    // M24
     
-        this.setup(0.6);
+    //to coincide with the cs/whipper, the bloom mechanics were redone
+    //meaning that the new values no longer go into the old system
+    //thus this is only a recreation
     
-        this.fireSound = "m24.fire";
-        this.dryFireSound = "eggk47.dryFire";
+    M24 = function (player, meshName) {
+        Gun.call(this, player, M24);
     
-        this.addSoundEvent('reload', 10, "m24.boltOpen");
-        this.addSoundEvent('reload', 75, "eggk47.insertMag");
-        this.addSoundEvent('reload', 94, "m24.boltClose");
+        this.ammo = {
+            rounds: 1,
+            capacity: 1,
+            reload: 1,
+            store: 20,
+            storeMax: 20,
+            pickup: 4
+        };
+    
+        this.hasScope = true;
+    
+        // Animation times
+        this.longReloadTime = 120;  // Full reload cycle
+        this.shortReloadTime = 120;  // Short cycle when one is in the chamber
+    
+        if (isClient) this.actor = new M24Actor(this, meshName);
     };
     
-    M24Actor.prototype = Object.create(GunActor.prototype);
-    M24Actor.prototype.constructor = GunActor;
+    M24.prototype = Object.create(Gun.prototype);
+    M24.prototype.constructor = Gun;
+    
+    M24.weaponName = 'M2DZ';
+    M24.standardMeshName = 'm24';
+    M24.rof = 15;
+    M24.recoil = 10; // Frames that must elapse before swapping or reloading can happen
+    M24.automatic = false;
+    M24.accuracy = 0; // Base inaccuracy of weapon (0-1)
+    // M24.stability = 0.1; // Controls accuracy loss per shot and rate of recovery (0-1)
+    M24.shotSpreadIncrement = 250;
+    M24.accuracySettleFactor = 0.98; // Controls accuracy loss per shot and rate of recovery (0-1) (previously stability: 0.1)
+    M24.stillSettleSpeed = 55;
+    M24.damage = 200;
+    M24.totalDamage = 15; // For main menu stats (because shotgun)
+    M24.range = 60;
+    M24.velocity = 0.6;
+    M24.tracer = 0;
+    
+    // Overrides from weapon analyzer (????????????????)
+    
+    M24.accuracy = 1;
+    // M24.stability = 0.15;
+    M24.damage = 250;
+    M24.totalDamage = M24.damage;
+    
+    M24.prototype.fireMunitions = function (pos, dir) {
+        Bullet.fire(this.player, pos, dir, M24);
+    };
+    
+    //actor
+    
+    var M24Actor;
+    
+    if (isClient) {
+        M24Actor = function (gun, meshName) {
+            GunActor.call(this, gun, meshName);
+            this.scopeFov = 0.4;
+            this.scopeY = 0.1 - 0.03507;
+        
+            this.setup(0.6);
+        
+            this.fireSound = "m24.fire";
+            this.dryFireSound = "eggk47.dryFire";
+        
+            this.addSoundEvent('reload', 10, "m24.boltOpen");
+            this.addSoundEvent('reload', 75, "eggk47.insertMag");
+            this.addSoundEvent('reload', 94, "m24.boltClose");
+        };
+        
+        M24Actor.prototype = Object.create(GunActor.prototype);
+        M24Actor.prototype.constructor = GunActor;
+    
+        CrackshotPlugin.registerListeners(plugins);
+    };
+} else {
+    console.log("Crackshot plugin not loading, Gun class not present!");
 };
