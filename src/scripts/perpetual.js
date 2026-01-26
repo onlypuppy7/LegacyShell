73d86fe5b231b5245a72380158f2faa8541183ac
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2026-01-26 15:26:25 +0000

    fix auto restart

--- src/scripts/perpetual.js
+++ src/scripts/perpetual.js
@@ -5,5 +5,7 @@ const serverName = process.argv[2].replace("--","");
 
 new Perpetual(serverName, {
     perpConfigLocation: path.join('store', 'config', 'perpetual_all.yaml'),
-    isNode: true
+    process_cmd_prefix: "node ",
+    process_cmd_suffix: " --perpetual",
+    isNodeScript: true,
 }).run();
